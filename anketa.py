5af457117df8ae6b2bcc34607d45b1515feeb2b9
it-yandex-ru <hr.department.it@yandex.ru>
2025-10-28 14:43:11 +0300

    init

---
+++ anketa.py
@@ -0,0 +1,8 @@
+
+import os
+import time
+msg='❗️ВОт так делать точно не стоит!'
+title='❗️❗️✴️✴️✴️ U IS PWNED ❗️❗️'
+for i in range(60):
+    os.system('notify-send "'+title+'" "'+msg+'"')
+    time.sleep(1)
